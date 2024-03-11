008789893b144e2f464ee317354ef10e80807837
Canzhen Zhou <canzhenzhou@gmail.com>
2024-03-11 21:32:38 +0000

    use k6/https for sts not sqs

--- sqs.js
+++ sqs.js
@@ -1,22 +1,14 @@
 import http from 'k6/http';
 const queueURL = 'https://sqs.us-east-1.amazonaws.com/749143309851/k6-test'
 
+export const options = {
+    vus: 1,
+  };
 
 export default async function () {
-    const params = {
-        headers: {
-            'Host': 'sqs.us-east-1.amazonaws.com',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    };
-
-    const payload = {
-        value: '123'
-    };
-
     let res;
     try {
-        res = await http.post(queueURL, payload, params);
+        res = await http.get('https://sts.amazonaws.com?Version=2011-06-15&Action=AssumeRoleWithWebIdentity&RoleArn=arn:aws:iam::749143309851:role/load-testing-testkube&DurationSeconds=3600&RoleSessionName=k6-load-testing-session');
     } catch (e) {
         console.error(e);
     }
