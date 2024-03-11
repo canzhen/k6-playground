043049ef09776dbcbea9b8eb747d96f7f44e1297
Canzhen Zhou <canzhenzhou@gmail.com>
2024-03-11 21:11:25 +0000

    print out the http post request response

--- sqs.js
+++ sqs.js
@@ -1,6 +1,3 @@
-import exec from 'k6/execution'
-
-// import { SQSClient } from 'https://jslib.k6.io/aws/0.11.0/sqs.js'
 import http from 'k6/http';
 const queueURL = 'https://sqs.us-east-1.amazonaws.com/749143309851/k6-test'
 
@@ -17,15 +14,13 @@ export default async function () {
         value: '123'
     };
 
-    await http.post(queueURL, payload, params)
-    // // If our test queue does not exist, abort the execution.
-    // const queuesResponse = await sqs.listQueues()
-    // if (queuesResponse.urls.filter((q) => q === queueURL).length == 0) {
-    //     exec.test.abort()
-    // }
-
-    // Send message to test queue
-    // await sqs.sendMessage(queueURL, JSON.stringify({value: '123'}));
+    let res;
+    try {
+        res = await http.post(queueURL, payload, params);
+    } catch (e) {
+        console.error(e);
+    }
+    console.log(res.body);
 }
 
 
