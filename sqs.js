7c99554409912fd3929c59b761066ec62fe373e3
Canzhen Zhou <canzhenzhou@gmail.com>
2024-03-11 20:19:02 +0000

    use aws sdk assumeRole

--- sqs.js
+++ sqs.js
@@ -1,18 +1,34 @@
 import exec from 'k6/execution'
 
-import { AWSConfig, SQSClient } from 'https://jslib.k6.io/aws/0.11.0/sqs.js'
-
-const awsConfig = new AWSConfig({
-    region: __ENV.AWS_REGION,
-    accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
-    secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
-    sessionToken: __ENV.AWS_SESSION_TOKEN,
-})
-
-const sqs = new SQSClient(awsConfig)
+import { SQSClient } from 'https://jslib.k6.io/aws/0.11.0/sqs.js'
+const AWS = require("aws-sdk");
 const testQueue = 'https://sqs.us-east-1.amazonaws.com/749143309851/k6-test'
 
+
 export default async function () {
+    const client = new AWS.STSClient({ region: "us-east-1" });
+    // Assume the role
+    try {
+        // Returns a set of temporary security credentials that you can use to
+        // access Amazon Web Services resources that you might not normally
+        // have access to.
+        const command = new AWS.AssumeRoleCommand({
+            // The Amazon Resource Name (ARN) of the role to assume.
+            RoleArn: "arn:aws:iam::749143309851:role/load-testing-testkube",
+            // An identifier for the assumed role session.
+            RoleSessionName: "k6-test-session",
+            // The duration, in seconds, of the role session. The value specified
+            // can range from 900 seconds (15 minutes) up to the maximum session
+            // duration set for the role.
+            DurationSeconds: 900,
+        });
+        const response = await client.send(command);
+        console.log(response);
+    } catch (err) {
+        console.error(err);
+    }
+
+    const sqs = new SQSClient({});
     // If our test queue does not exist, abort the execution.
     const queuesResponse = await sqs.listQueues()
     if (queuesResponse.urls.filter((q) => q === testQueue).length == 0) {
@@ -22,3 +38,5 @@ export default async function () {
     // Send message to test queue
     await sqs.sendMessage(testQueue, JSON.stringify({value: '123'}));
 }
+
+
