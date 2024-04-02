7af7c20857eef91a587c556f360db5573d245a3a
Canzhen (Jojo) Zhou <czhou@turo.com>
2024-04-02 23:06:36 +0000

    remove serviceaccount credentials

--- load-test/k6-test.js
+++ load-test/k6-test.js
@@ -14,66 +14,70 @@ const AWS_SQS_NAME = getEnvValue("AWS_SQS_NAME");
 const AWS_ROLE_ARN = getEnvValue("AWS_ROLE_ARN", "");
 const AWS_REGION = getEnvValue("AWS_REGION", "us-east-1");
 
-const AWS_WEB_IDENTITY_TOKEN = open(
-  "/var/run/secrets/eks.amazonaws.com/serviceaccount/token",
-  "utf-8"
-);
-const ASSUME_ROLE_WITH_WEB_IDENTITY_URL =
-  AWS_WEB_IDENTITY_TOKEN && AWS_ROLE_ARN
-    ? `https://sts.amazonaws.com/?Action=AssumeRoleWithWebIdentity&RoleArn=${AWS_ROLE_ARN}&WebIdentityToken=${AWS_WEB_IDENTITY_TOKEN}&RoleSessionName=app1&Version=2011-06-15&DurationSeconds=3600`
-    : "";
+// const AWS_WEB_IDENTITY_TOKEN = open(
+//   "/var/run/secrets/eks.amazonaws.com/serviceaccount/token",
+//   "utf-8"
+// );
+// const ASSUME_ROLE_WITH_WEB_IDENTITY_URL =
+//   AWS_WEB_IDENTITY_TOKEN && AWS_ROLE_ARN
+//     ? `https://sts.amazonaws.com/?Action=AssumeRoleWithWebIdentity&RoleArn=${AWS_ROLE_ARN}&WebIdentityToken=${AWS_WEB_IDENTITY_TOKEN}&RoleSessionName=app1&Version=2011-06-15&DurationSeconds=3600`
+//     : "";
 
 export const options = {
   scenarios: {
-    // indexing: {
-    //   executor: "per-vu-iterations",
-    //   exec: "indexing",
-    //   vus: INDEXING_VU,
-    //   iterations: INDEXING_ITERATIONS,
-    //   maxDuration: INDEXING_MAX_DURATION,
-    //   env: {
-    //     SCENARIO: "indexing",
-    //     AWS_SQS_NAME: AWS_SQS_NAME,
-    //     QUEUE_URL: AWS_SQS_URL + AWS_SQS_NAME,
-    //   },
-    // },
-    search: {
+    indexing: {
       executor: "per-vu-iterations",
-      exec: "search",
-      vus: 1,
-      iterations: 1, 
-      maxDuration: '1s',
+      exec: "indexing",
+      vus: INDEXING_VU,
+      iterations: INDEXING_ITERATIONS,
+      maxDuration: INDEXING_MAX_DURATION,
       env: {
-        SCENARIO: "search",
-        SEARCH_REQUEST_URL: getEnvValue("SEARCH_REQUEST_URL"),
-        SEARCH_REQUEST_USER_AGENT: getEnvValue(
-          "SEARCH_REQUEST_USER_AGENT",
-          "Turo/22.44.1 (iPhone; iOS 15.0; Scale/2.00)"
-        ),
-        SEARCH_REQUEST_REFERRER: getEnvValue("SEARCH_REQUEST_REFERRER"),
+        SCENARIO: "indexing",
+        AWS_SQS_NAME: AWS_SQS_NAME,
+        QUEUE_URL: AWS_SQS_URL + AWS_SQS_NAME,
       },
     },
+    // search: {
+    //   executor: "per-vu-iterations",
+    //   exec: "search",
+    //   vus: 1,
+    //   iterations: 1, 
+    //   maxDuration: '1s',
+    //   env: {
+    //     SCENARIO: "search",
+    //     SEARCH_REQUEST_URL: getEnvValue("SEARCH_REQUEST_URL"),
+    //     SEARCH_REQUEST_USER_AGENT: getEnvValue(
+    //       "SEARCH_REQUEST_USER_AGENT",
+    //       "Turo/22.44.1 (iPhone; iOS 15.0; Scale/2.00)"
+    //     ),
+    //     SEARCH_REQUEST_REFERRER: getEnvValue("SEARCH_REQUEST_REFERRER"),
+    //   },
+    // },
   },
 };
 
 export function setup() {
-  const res = http
-    .post(ASSUME_ROLE_WITH_WEB_IDENTITY_URL, null, {
-      headers: { Accept: "application/json" },
-    })
-    .json();
-  const credentials =
-    res.AssumeRoleWithWebIdentityResponse.AssumeRoleWithWebIdentityResult
-      .Credentials;
+  // const res = http
+  //   .post(ASSUME_ROLE_WITH_WEB_IDENTITY_URL, null, {
+  //     headers: { Accept: "application/json" },
+  //   })
+  //   .json();
+  // const credentials =
+  //   res.AssumeRoleWithWebIdentityResponse.AssumeRoleWithWebIdentityResult
+  //     .Credentials;
 
-  const awsConfig = new AWSConfig({
-    region: AWS_REGION,
-    accessKeyId: credentials.AccessKeyId,
-    secretAccessKey: credentials.SecretAccessKey,
-    sessionToken: credentials.SessionToken,
-  });
+  // const awsConfig = new AWSConfig({
+  //   region: AWS_REGION,
+  //   accessKeyId: credentials.AccessKeyId,
+  //   secretAccessKey: credentials.SecretAccessKey,
+  //   sessionToken: credentials.SessionToken,
+  // });
 
-  return { awsConfig: awsConfig };
+  // return { awsConfig: new AWSConfig({
+  //     region: __ENV.AWS_REGION,
+  //     accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
+  //     secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
+  //   })};
 }
 
 export function indexing(data) {
--- load-test/search/search.js
+++ load-test/search/search.js
@@ -7,38 +7,38 @@ export function searchFunc() {
   if (__ENV.SCENARIO !== "search")
     exec.test.abort("aborting because scenario is not search");
 
-  // const payload = JSON.stringify(randomSearchPayload(), null, 2);
-  // console.log('---------------------------- search payload ----------------------------');
-  // console.log(payload);
-  const payload = JSON.stringify({
-    "filters":{
-      "location":{
-        "country":"US",
-        "type":"area",
-        "point":{
-          "lat":"34.0549076",
-          "lng":"-118.2426430",
-        },
-      },
-      "engines":[],
-      "makes":[],
-      "models":[],
-      "dates":{
-        "end":"2024-05-16T10:00",
-        "start":"2024-05-13T10:00",
-      },
-      "tmvTiers":[],
-      "features":[],
-      "types":[],
-    },
-    "sorts":{
-      "direction":"ASC",
-      "type":"RELEVANCE",
-    },
-    "searchContext": {
-      "searchId": "12345",
-    }
-  }, null, 2);
+  const payload = JSON.stringify(randomSearchPayload(), null, 2);
+  console.log('---------------------------- search payload ----------------------------');
+  console.log(payload);
+  // const payload = JSON.stringify({
+  //   "filters":{
+  //     "location":{
+  //       "country":"US",
+  //       "type":"area",
+  //       "point":{
+  //         "lat":"34.0549076",
+  //         "lng":"-118.2426430",
+  //       },
+  //     },
+  //     "engines":[],
+  //     "makes":[],
+  //     "models":[],
+  //     "dates":{
+  //       "end":"2024-05-16T10:00",
+  //       "start":"2024-05-13T10:00",
+  //     },
+  //     "tmvTiers":[],
+  //     "features":[],
+  //     "types":[],
+  //   },
+  //   "sorts":{
+  //     "direction":"ASC",
+  //     "type":"RELEVANCE",
+  //   },
+  //   "searchContext": {
+  //     "searchId": "12345",
+  //   }
+  // }, null, 2);
   const res = http.post(__ENV.SEARCH_REQUEST_URL, payload, {
     headers: {
       "Content-Type": "application/json",
